9f3a9b67cd3581421d90e528b1108917598d44ed
XUE <xy7821665@126.com>
2023-08-24 14:12:18 +0800

    bak

---
+++ src/router/page/index.ts
@@ -0,0 +1,20 @@
+export default [
+    {
+        path: '/home',
+        name: 'home',
+        component: () => import('@/views/HomeView.vue')
+    },
+    {
+        path: '/about',
+        name: 'about',
+        // route level code-splitting
+        // this generates a separate chunk (About.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import('@/views/AboutView.vue')
+    },
+    {
+        path: '/menu',
+        name: 'menu',
+        component: () => import('@/views/MenuView.vue')
+    }
+]
